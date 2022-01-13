18c8745f2a59ec24e348323f60163bde13cef9ce
Distortions81 <carlotto81@gmail.com>
2022-01-13 11:21:50 -0700

    move info dir

---
+++ FactBanSync/glob.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io/fs"
+	"os"
+)
+
+var defaultListURL = "https://raw.githubusercontent.com/Distortions81/FactBanSync/master/server-list.json"
+var defaultConfigPath = "server-config.json"
+var defaultBanFile = "server-banlist.json"
+var defaultServerListFile = "server-list.json"
+var defaultOurBansFile = "our-bans.json"
+var defaultLogPath = "logs"
+var defualtFetchBansInterval = 60        //Minutes
+var defualtWatchInterval = 5             //Seconds
+var defualtRefreshListInterval = 60 * 24 //Minutes
+var serverConfig serverConfigData
+var serverList serverListData
+var configPath string
+var logDesc *os.File
+var banData []banDataData
+var serverRunning = true
+
+var initialStat fs.FileInfo
---
+++ FactBanSync/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+)
+
+const version = "0.0.1"
+
+func main() {
+
+	//Launch arguments
+	configPath = *flag.String("configPath", defaultConfigPath, "config file path")
+	flag.Parse()
+
+	readConfigFile()
+	writeConfigFile() //To clean up formatting
+
+	startLog()
+	log.Println(fmt.Sprintf("FactBanSync v%v", version))
+	updateServerList()
+
+	readServerListFile()
+	readServerBanList()
+	writeBanListFile() //To clean up formatting
+
+	var LastFetchBans = time.Now()
+	var LastWatch = time.Now()
+	var LastRefresh = time.Now()
+
+	for serverRunning {
+		time.Sleep(time.Second)
+
+		if time.Since(LastFetchBans).Minutes() >= float64(serverConfig.FetchBansInterval) {
+			LastFetchBans = time.Now()
+
+			//Fetch bans (TODO)
+		}
+		if time.Since(LastWatch).Seconds() >= float64(serverConfig.WatchInterval) {
+			LastWatch = time.Now()
+
+			WatchBanFile()
+		}
+		if time.Since(LastRefresh).Minutes() >= float64(serverConfig.RefreshListInterval) {
+			LastRefresh = time.Now()
+
+			updateServerList()
+		}
+	}
+}
