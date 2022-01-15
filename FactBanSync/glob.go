c95bcf06dbd405655d0acfd89dd55e3082324db1
distortions81 <distortions81@gmail.com>
2022-01-15 16:08:20 -0700

    max banlist size

--- FactBanSync/glob.go
+++ FactBanSync/glob.go
@@ -12,31 +12,35 @@ var defaultListURL = "https://raw.githubusercontent.com/Distortions81/Factorio-C
 var timeFormat = time.RFC822Z
 
 //Default file names
-var defaultName = "Default"
-var defaultBanFile = ""
-var defaultWebPort = 8080
-var defaultDataDir = "data"
-var defaultConfigPath = "data/server-config.json"
-var defaultServerListFile = "data/server-list.json"
-var defaultRCONFile = "data/server-rcon.json"
-var defaultLogMonitorFile = "data/log-monitor.json"
-var banFileWebName = "server-banlist.json"
+const defaultName = "Default"
+const defaultBanFile = ""
+const defaultWebPort = 8080
+const defaultDataDir = "data"
+const defaultConfigPath = "data/server-config.json"
+const defaultServerListFile = "data/server-list.json"
+const defaultRCONFile = "data/server-rcon.json"
+const defaultLogMonitorFile = "data/log-monitor.json"
+const defaultFileWebName = "server-banlist.json"
 
 //Default directories
-var defaultLogDir = "data/logs"
-var defaultBanFileDir = "data/banCache"
+const defaultLogDir = "data/logs"
+const defaultBanFileDir = "data/banCache"
 
 //Default delay times
-var defaultFetchBansSeconds = 15        //Seconds
-var defaultWatchSeconds = 10            //Seconds
-var defaultRefreshListMinutes = 60 * 24 //One a day
+const defaultFetchBansSeconds = 300
+const defaultWatchSeconds = 10
+const defaultRefreshListMinutes = 60 * 12
+
+//Max banlist size
+const defaultMaxBanListSize = 250
 
 //Global vars
 var serverConfig serverConfigData
 var serverList serverListData
 var configPath string
 var logDesc *os.File
-var banData []banDataType
+var ourBanData []banDataType
+var compBanData []banDataType
 var logsToMonitor []LogMonitorData
 var cachedBanListGz []byte
 var cachedBanList []byte
