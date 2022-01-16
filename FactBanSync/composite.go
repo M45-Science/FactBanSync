e3d9d4cfdb22b0425320ea132317b9aca50b3320
Distortions81 <carlotto81@gmail.com>
2022-01-16 08:47:51 -0700

    Don't composite revoked bans, sort by new

--- FactBanSync/composite.go
+++ FactBanSync/composite.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"sort"
 	"strconv"
+	"time"
 )
 
 func compositeBans() {
@@ -14,18 +15,22 @@ func compositeBans() {
 
 	dupes := 0
 	for _, server := range serverList.ServerList {
+		//Only if subscribed, and skip self
 		if server.Subscribed && server.Name != serverConfig.Name {
 			for _, ban := range server.BanList {
-				found := false
-				for _, iban := range ourBanData {
-					if iban.UserName == ban.UserName {
-						found = true
-						dupes++
-						break
+				//Don't composite revoked bans
+				if !ban.Revoked {
+					found := false
+					for _, iban := range ourBanData {
+						if iban.UserName == ban.UserName {
+							found = true
+							dupes++
+							break
+						}
 					}
-				}
-				if !found {
-					compositeBanlist = append(compositeBanlist, ban)
+					if !found {
+						compositeBanlist = append(compositeBanlist, ban)
+					}
 				}
 			}
 		}
@@ -33,17 +38,24 @@ func compositeBans() {
 
 	log.Println("Found " + strconv.Itoa(len(compositeBanlist)) + " bans, " + strconv.Itoa(dupes) + " duplicates")
 
-	//Sort by local add
+	//Sort by time added, new to old
 	sort.Slice(compositeBanlist, func(i, j int) bool {
-		return compositeBanlist[i].LocalAdd > compositeBanlist[j].LocalAdd
+		aTime, erra := time.Parse(timeFormat, compositeBanlist[i].LocalAdd)
+		bTime, errb := time.Parse(timeFormat, compositeBanlist[j].LocalAdd)
+		if erra != nil || errb != nil {
+			log.Println("Error parsing time: " + erra.Error())
+			return false
+		}
+		return aTime.Before(bTime)
 	})
 
+	//Cut list to size, new entries are at the start
 	compBanData = []banDataType{}
 	for bpos, ban := range compositeBanlist {
 		if bpos < serverConfig.MaxBanlistSize {
 			compBanData = append(compBanData, ban)
 		} else {
-			log.Println("Banlist size exceeded, truncating")
+			log.Println("Banlist size exceeded, truncating...")
 			break
 		}
 	}
